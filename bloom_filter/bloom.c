a3c9440ccdf1b55a42e8797c9fd264654f6fb26c
Amy <amy.mae.lee@gmail.com>
2016-02-15 02:00:46 -0500

    add Bit Operations

--- bloom_filter/bloom.c
+++ bloom_filter/bloom.c
@@ -23,6 +23,64 @@ int main(){
     return 0;
 }
 
+/*******************
+ Bit Operations
+*******************/
+// storing into array
+void set_bit(bloom_filter_t *B, index_t i){
+    // do check here to make sure i < size
+    if (i >= B->size){
+        printf("Index out of bounds!");
+        exit(1);
+    }
+
+    // set bit i in table
+    // do integer division to find the array location, sizeof gives bytes, multiply by 8 to get bits
+    index_t index = i / (sizeof(index_t)*8);
+    // do modulo to find the bit offset in the int
+    index_t bit = i % (sizeof(index_t)*8);
+
+    index_t mask = 1;
+
+    // create mask for that bit offset... initialize int64 to 1... bitwise left shift i places: << i
+    mask = mask << bit;
+
+    // OR mask with current int and store the result of the OR back at the same location of the array
+    B->table[index] = mask | B->table[index];
+}
+
+
+// should return 1 if bit is set, 0 if not. Checking if bit is set or not.
+index_t get_bit(bloom_filter_t *B, index_t i){
+    // do check here to make sure i < size
+    if (i >= B->size){
+        printf("Index out of bounds!");
+        exit(1);
+    }
+
+    // get bit i in table
+    // do integer division to find the array location, sizeof gives bytes, multiply by 8 to get bits
+    index_t index = i / (sizeof(index_t)*8);
+    // do modulo to find the bit offset in the int
+    index_t bit = i % (sizeof(index_t)*8);
+
+    index_t mask = 1;
+
+    // create mask for that bit offset... initialize int64 to 1... bitwise left shift i places: << i
+    mask = mask << bit;
+
+    // OR mask with current int and store the result of the OR back at the same location of the array
+    index_t result = mask & B->table[index];
+
+    // If result is 0, then bit was not set. If it is NOT 0, then bit was set
+    return result;
+}
+
+/****************
+ API / Interface
+****************/
+
+
 /****************
  Hash Functions
 ****************/
@@ -39,7 +97,7 @@ index_t hash1(bloom_filter_t *B, key_t k){
 
     for(i = 0; i < len; str++, i++)
     {
-        // left-shift over bits so that the 5 right-most bits of the hash are 0's, adding the number value associated to k
+        // left-shift over bits so that the 5 right-most bits of the hash are 0's, then add the number value represented by those bits to k
         // then right-shift hash by 2 and adding the associated value
         // then do a bitwise XOR (not equals) with the original hash: same as hash ^=
         hash = hash ^ ( ((hash << 5) + (k) + (hash >> 2)) );
